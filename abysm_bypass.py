606f0fe24c2f8ef27c737d1db111a7d971aef8ce
DevD-Z <toonrog8@gmail.com>
2026-06-09 23:44:13 +0700

    Update abysm_bypass.py

--- abysm_bypass.py
+++ abysm_bypass.py
@@ -3,7 +3,7 @@ import requests
 
 def bypass_link(url):
     api_url = f"https://api.izen.lol/v1/bypass?url={url}"
-    headers = {"x-api-key": "c2cb527f-464f-4e47-a3f2-b171b835d46e"}
+    headers = {"x-api-key": api_key}
     response = requests.get(api_url, headers=headers)
     if response.status_code != 200:
         print("Abysm API call failed")
